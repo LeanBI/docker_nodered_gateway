1795be89c6c4595179ea0da00c5b722c2c0c43c1
sebastien.brennion <sebastien.brennion@LEANBI-Ersatz>
2017-07-11 10:39:49 +0200

    d

--- postgres/init.sh
+++ postgres/init.sh
@@ -10,12 +10,26 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE measures;
     GRANT ALL PRIVILEGES ON DATABASE measures TO admin;
     CONNECT measures
-    CREATE TABLE public.measures
-		(
-		    "timestamp" timestamp without time zone DEFAULT now()
-		)
-		WITH (
-		    OIDS = FALSE
-		);
-	ALTER TABLE public.measures OWNER to admin;	
+	CREATE TABLE public.measures
+	(
+	    "timestamp" timestamp without time zone,
+	    totalmem bigint,
+	    freemem bigint,
+	    memusage numeric,
+	    cpu0_speed numeric,
+	    cpu0_sys numeric,
+	    cpu1_speed numeric,
+	    cpu1_sys numeric,
+	    cpu1_idle numeric,
+	    loadavg_0 numeric,
+	    loadavg_1 numeric,
+	    loadavg_2 numeric
+	)
+	WITH (
+	    OIDS = FALSE
+	)
+	TABLESPACE pg_default;
+	
+	ALTER TABLE public.measures
+	    OWNER to admin;
 EOSQL
