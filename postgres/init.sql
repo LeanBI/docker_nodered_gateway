40bf588465b8bd3a493081e204cbb778e765b9e3
sebastien.brennion <sebastien.brennion@LEANBI-Ersatz>
2017-07-17 16:41:13 +0200

    d

--- postgres/init.sql
+++ postgres/init.sql
@@ -21,4 +21,21 @@ CREATE EXTENSION madlib CASCADE;
 	)
 	TABLESPACE pg_default;
 	
+CREATE OR REPLACE VIEW public.ml_source AS
+ SELECT measures."timestamp",
+    measures.totalmem::double precision AS totalmem,
+    measures.freemem::double precision AS freemem,
+    measures.memusage::double precision AS memusage,
+    measures.cpus_0_speed::double precision AS cpus_0_speed,
+    measures.cpus_0_sys::double precision AS cpus_0_sys,
+    measures.cpus_1_speed::double precision AS cpus_1_speed,
+    measures.cpus_1_sys::double precision AS cpus_1_sys,
+    measures.cpus_1_idle::double precision AS cpus_1_idle,
+    measures.loadavg_0::double precision AS loadavg_0,
+    measures.loadavg_1::double precision AS loadavg_1,
+    measures.loadavg_2::double precision AS loadavg_2,
+    measures.prediction::double precision AS prediction
+   FROM measures;
 
+ALTER TABLE public.ml_source
+    OWNER TO iot;
